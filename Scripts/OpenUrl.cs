1ca31d17d4a30a9868524341f1964697cc066239
Cleyton Palauro <cleytonresc@gmail.com>
2019-06-07 10:15:41 -0300

    Add files via upload

---
+++ Scripts/OpenUrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    public string address;
+
+	void Start () { }
+	
+	void Update () { }
+
+    public void OpenAddress()
+    {
+        Application.OpenURL(address);
+    }
+
+    public void OpenAddress(string weblink)
+    {
+        Application.OpenURL(weblink);
+    }
+}
